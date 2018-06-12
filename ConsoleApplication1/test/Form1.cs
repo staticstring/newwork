80011fbcd3df6064c0ec2bc7a7d06efc662454ee
staticstring <453617263@qq.com>
2018-06-12 22:50:40 +0800

    test

---
+++ ConsoleApplication1/test/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(Test);
+            th.IsBackground = true;
+            th.Start();
+            //Test();
+        }
+
+        private void Test() {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine(i);
+             
+            }
+        }
+    }
+}
